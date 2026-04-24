9f21abff2c05d4a3b2b43347fb9e580f0cfde51f
Loraelfeky <157986613+Loraelfeky@users.noreply.github.com>
2026-04-24 14:29:27 +0300

    Add files via upload

---
+++ carfactory/Engine.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Souq al computer
+ */
+
+public interface Engine {
+    void start();
+    void stop();
+    void increase();
+    void decrease();
+    void checkSpeed(int speed);
+}
+    
+
